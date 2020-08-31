b074352d7016bd4a5f1600c1d643d8e3b5ded8fa
Andrea <andreasiljanovska57@gmail.com>
2020-08-31 12:58:39 +0200

    routing

--- myfirstapp/src/app/app.module.ts
+++ myfirstapp/src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http'
+import { Routes, RouterModule } from '@angular/router'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -7,19 +9,45 @@ import { NavigationBarComponent } from './navigation-bar/navigation-bar.componen
 import { PropertyCardsComponent } from './property-cards/property-cards.component';
 import { PropertyListComponent } from './property-list/property-list.component';
 import { AddPropertyComponent } from './add-property/add-property.component';
+import { HousingService } from './services/housing.service';
+import { PropertyDetailsComponent } from './property-details/property-details.component';
+
+const appRoutes: Routes = [
+  {
+    path:'rent-property',
+    component: PropertyListComponent
+  },
+  {
+    path: 'property-details/:id',
+    component: PropertyDetailsComponent
+  },
+  {
+    path:'',
+    component: PropertyListComponent
+  },
+  
+  {
+    path: 'add-property', 
+    component: AddPropertyComponent
+  }
+]
+
 @NgModule({
-  declarations: [					
+  declarations: [						
     AppComponent,
       NavigationBarComponent,
       PropertyCardsComponent,
       PropertyListComponent,
-      AddPropertyComponent
+      AddPropertyComponent,
+      PropertyDetailsComponent
    ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    RouterModule.forRoot(appRoutes)
   ],
-  providers: [],
+  providers: [HousingService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
